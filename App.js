fa8f58f7ac9fd250a3666c2481e692ab82b329e8
chintrinh-groove <chin.trinh@groovetechnology.com>
2021-06-06 20:53:31 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,115 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import {StyleSheet, View, Pressable} from 'react-native';
+import {RingPicker} from './src/components/RingPicker';
+
+const BUTTON_SIZE = 100;
+const CIRCLE1_SIZE = 200;
+const CIRCLE2_SIZE = 300;
+const CIRCLE3_SIZE = 400;
+
+const data1 = [...Array(3).keys()];
+const data2 = [...Array(3).keys()];
+const data3 = [...Array(3).keys()];
+
+const App = () => {
+  const [visible1Circle, setCircle1Visible] = useState(true);
+  const [visible2Circle, setCircle2Visible] = useState(true);
+  const [visible3Circle, setCircle3Visible] = useState(true);
+
+  const openCircle1 = () => setCircle1Visible(true);
+  const closeCircle1 = () => setCircle1Visible(false);
+  const openCircle2 = () => setCircle2Visible(true);
+  const closeCircle2 = () => setCircle2Visible(false);
+  const openCircle3 = () => setCircle3Visible(true);
+  const closeCircle3 = () => setCircle3Visible(false);
+
+  const toggleCircle1 = () => {
+    if (visible1Circle) {
+      closeCircle1();
+      closeCircle2();
+      closeCircle3();
+      return;
+    }
+    openCircle1();
+  };
+  const toggleCircle2 = () => {
+    if (visible2Circle) {
+      closeCircle2();
+      closeCircle3();
+      return;
+    }
+    openCircle2();
+  };
+  const toggleCircle3 = () => {
+    if (visible3Circle) {
+      closeCircle3();
+      return;
+    }
+    openCircle3();
+  };
+  const toggleCircle4 = () => console.log('END');
+
+  return (
+    <View style={styles.container}>
+      <RingPicker
+        visible={visible3Circle}
+        contentContainerStyle={{
+          marginBottom: BUTTON_SIZE,
+        }}
+        width={CIRCLE3_SIZE}
+        data={data3}
+        onPress={toggleCircle4}
+      />
+      <RingPicker
+        visible={visible2Circle}
+        contentContainerStyle={{
+          marginBottom: BUTTON_SIZE,
+        }}
+        width={CIRCLE2_SIZE}
+        data={data2}
+        onPress={toggleCircle3}
+      />
+      <RingPicker
+        visible={visible1Circle}
+        contentContainerStyle={{
+          marginBottom: BUTTON_SIZE,
+        }}
+        width={CIRCLE1_SIZE}
+        data={data1}
+        onPress={toggleCircle2}
+      />
+      <Pressable
+        style={{
+          position: 'absolute',
+          flex: 1,
+          bottom: -BUTTON_SIZE / 2,
+          width: BUTTON_SIZE,
+          aspectRatio: 1,
+          borderRadius: BUTTON_SIZE / 2,
+          borderColor: 'red',
+          borderWidth: 10,
+          marginBottom: BUTTON_SIZE,
+        }}
+        onPress={toggleCircle1}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
---
+++ src/components/RingPicker/index.js
@@ -0,0 +1,218 @@
+import React, {useRef, useState} from 'react';
+import {
+  Animated,
+  PanResponder,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const RADIUS = 100;
+const buttons = [Array(5).keys()];
+
+export function RingPicker({
+  visible,
+  width = RADIUS,
+  contentContainerStyle,
+  data = [],
+  onPress = () => {},
+  children,
+}) {
+  const pan1 = useRef(new Animated.ValueXY()).current;
+
+  // const pan = useRef(new Animated.Value(0)).current;
+
+  function getDegree(anchor, point, type = 'right') {
+    const directions = {
+      right: 0,
+      top: 90,
+      left: 180,
+      bottom: 270,
+    };
+    const dy = anchor.y - point.y;
+    const dx = anchor.x - point.x;
+    const angle = Math.atan2(dy, dx);
+    const degree = (angle * 180) / Math.PI + 180;
+
+    return degree;
+  }
+
+  const panResponder = useRef(
+    PanResponder.create({
+      // Ask to be the responder:
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      // onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      // onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      // onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // The gesture has started. Show visual feedback so the user knows
+        // what is happening!
+        // gestureState.d{x,y} will be set to zero now
+        console.log('onPanResponderMove');
+        console.log(
+          'Location',
+          evt.nativeEvent.locationX,
+          evt.nativeEvent.locationY,
+        );
+        console.log('Move', gestureState.moveX, gestureState.moveY);
+        console.log('D', gestureState.dx, gestureState.dy);
+        // const anchor = {
+        //   x: evt.nativeEvent.locationX,
+        //   y: evt.nativeEvent.locationY,
+        // };
+        // const point = {
+        //   x: RADIUS,
+        //   y: RADIUS,
+        // };
+        // const degree = getDegree(anchor, point);
+        // console.log('Degree', degree);
+        // pan.setOffset(degree + 75);
+        pan1.setOffset({
+          x: pan1.x._value,
+          y: pan1.y._value,
+        });
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+        // pan.setValue({
+        //   x: gestureState.dx,
+        //   y: gestureState.dy,
+        // });
+        console.log('onPanResponderMove');
+        // console.log(
+        //   'Location',
+        //   evt.nativeEvent.locationX,
+        //   evt.nativeEvent.locationY,
+        // );
+        // console.log('Move', gestureState.moveX, gestureState.moveY);
+        // console.log('D', gestureState.dx, gestureState.dy);
+        // const anchor = {
+        //   x: evt.nativeEvent.locationX,
+        //   y: evt.nativeEvent.locationY,
+        // };
+        // const point = {
+        //   x: RADIUS,
+        //   y: RADIUS,
+        // };
+        // const degree = getDegree(anchor, point);
+        // console.log('Degree', Math.round(degree));
+        // pan.setValue(degree);
+        pan1.setValue({
+          x: gestureState.dx,
+          y: gestureState.dy,
+        });
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onPanResponderRelease: (evt, gestureState) => {
+        // The user has released all touches while this view is the
+        // responder. This typically means a gesture has succeeded
+        console.log('onPanResponderRelease');
+        // pan.flattenOffset();
+        pan1.flattenOffset();
+      },
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true;
+      },
+    }),
+  ).current;
+
+  if (!visible) return null;
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          bottom: -width / 2,
+        },
+        contentContainerStyle,
+      ]}
+      {...panResponder.panHandlers}>
+      <Animated.View
+        style={[
+          styles.box,
+          {width, borderRadius: width},
+          {
+            transform: [
+              {
+                rotate: pan1.x.interpolate({
+                  inputRange: [-width * 2, 0, width * 2],
+                  outputRange: ['-180deg', '0deg', '180deg'],
+                  extrapolate: 'clamp',
+                }),
+              },
+            ],
+          },
+        ]}>
+        {data.length > 0 &&
+          data.map((item, index) => {
+            const percent = 360 / data.length;
+            return (
+              <Pressable
+                key={index}
+                style={{
+                  position: 'absolute',
+                  borderColor: 'blue',
+                  borderWidth: 1,
+                  transform: [
+                    {
+                      translateY: index === 0 ? -width / 2 : 0,
+                    },
+                    {
+                      translateX: index === 1 ? width / 2 : 0,
+                    },
+                    {
+                      translateX: index === 2 ? -width / 2 : 0,
+                    },
+                    {
+                      rotate: `${percent * index}deg`,
+                    },
+                  ],
+                }}
+                onPress={onPress}>
+                <Text>{item}</Text>
+              </Pressable>
+            );
+          })}
+        {children}
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    bottom: RADIUS / 2,
+  },
+  titleText: {
+    fontSize: 14,
+    lineHeight: 24,
+    fontWeight: 'bold',
+  },
+  box: {
+    width: RADIUS * 2,
+    aspectRatio: 1,
+    borderRadius: RADIUS,
+    borderTopColor: 'red',
+    borderRightColor: 'orange',
+    borderLeftColor: 'pink',
+    borderBottomColor: 'purple',
+    borderWidth: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
